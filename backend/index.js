4cfa013939beff9d734dd681bc72af20a1f04d66
k-ankit07 <07.kankit@gmail.com>
2025-02-19 14:07:50 +0530

    allowed headers

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 app.use(
   cors({
     origin: ["https://younglabs.vercel.app/", "http://localhost:5173"],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
